d3419d59566fe0df43754d4c59492aeabea3995d
Kseniya Bogoslovskaya <fromkseniya@gmail.com>
2019-08-14 08:28:07 -0400

    ✨ Diagnosis now being diplayed on html page

--- static/js/app.js
+++ static/js/app.js
@@ -26,23 +26,23 @@ function createDropdown() {
 function submitdropdownvalue(newvalue) {
   d3.select("#analyze").property("value", newvalue);
 }
+// Saving the table and results as variables
+var table = d3.select("#uwds");
+var tableCytology = d3.select("#cds");
+var resultDisplay = d3.select("#diagnosis");
+
+// Clear values for existing feature table and diagnosis
+table.html("");
+tableCytology.html("");
+resultDisplay.html("&nbsp;");
 
 function selectPatient(patientID) {
   /**
-    /* Populates form with features from selected patient
-    /* @param {string}    patientID    ID of selected patient 
-    /* patient in feature array
+    * Populates form with features from selected patient
+    * @param {string}    patientID    ID of selected patient 
+    * patient in feature array
     */
 
-  // Saving the table and results as variables
-  var table = d3.select("#uwds");
-  var tableCytology = d3.select("#cds");
-  var resultDisplay = d3.select("#diagnosis");
-
-  // Clear values for existing feature table and diagnosis
-  table.html("");
-  tableCytology.html("");
-  resultDisplay.html("&nbsp;");
 
   // url for wisconsin analysis
   var featuresURL = `/features/${patientID}`;
@@ -73,96 +73,39 @@ function selectPatient(patientID) {
   });
   printResult(patientID);
 }
-  // // Fetch results and display in #diagnosis
-  // d3.json(analysisURL).then(function(resultsWisconsin) {
-  //   resultDisplay.html(resultsWisconsin);
-  // });
 
-  // // Save results in variables
-  // let diagnosisC = d3.json(predictURL).then(function(resultsCytology) {
-  //   console.log(resultsCytology);
-  // });
+// Have to use async await function to wait for both diagnosis to return the results
+const fetchMLdata = async url => {
+  const data = await d3.json(url);
+  return data;
+};
 
-  // let diagnosisW = d3.json(analysisURL).then(function(resultsW) {
-  //   console.log(resultsW);
-  // });
+// Saving a function in a varaible 'printResult' and 'printResult' is a func from above
+const printResult = async patientID => {
+  // Results from promise that fethes the data by passing flask url as an argument -> saving in a var
+  const data = await Promise.all([
+    await fetchMLdata(`/analyze/${patientID}`),
+    await fetchMLdata(`/predict/${patientID}`)
+  ]);
+  console.log(data); // Save results in the objects
 
-  // // // if statement to spit out the diagnosis from both datasets
-  // // if (diagnosisC === "Malignant" || diagnosisW === "Malignant") {
-  // //   console.log("Bummer results");
-  // // } else {
-  // //   console.log("Good");
-  // // };
+  uwResult = data[0];
+  cResult = data[1]; // if statement to return the diagnosis
 
-  // async function fethcURLs() {
-  //   try {
-  //     var data = await Promise.all([
-  //       fetch(predictURL).then((response) => response.json()),
-  //       fetch(analysisURL).then((response) => response.json()),
-  //     ])
-  //   }
+  // Saving result in a var to display it on the page
+  var final = "";
 
-  // }
+  if (uwResult === "Malignant" || cResult === "Malignant") {
+    // console.log("Malignant");
+    final = "Malignant";
+  } else {
+    // console.log("Benigh");
+    final = "Benign";
+  }
 
-  // // Attempt 3
-  // async function predictFunc() {
-
-  //   // read our cytology
-  //   let responseCytology = await fetch(`/predict/${patientID}`);
-  //   let awaitCytology = await responseCytology.json();
-
-  //     // read our wisconsin
-  //   let responseWisconsin = await fetch(`/analyze/${patientID}`);
-  //   let awaitWisconsin = await responseWisconsin.json();
-
-  //   await new Promise((resolve, reject) => setTimeout(resolve, 3000));
-
-  //   return awaitCytology;
-
-  //   // Unreachable
-  //   // return awaitWisconsin;
-
-  // }
-  // predictFunc()
-
-  // // Attempt 4
-  // async function damn() {
-  //   let results = await Promise.all([
-  //     fetch(`/analyze/${patientID}`),
-  //     fetch(`/predict/${patientID}`),
-  //   ]);
-
-  //   console.log(results);
-  //   };
-  // damn();
-
-  // let test = fetch(`/analyze/${patientID}`);
-  // console.log(test);
-
-  // Have to use async await function to wait for both diagnosis to return the results
-  const fetchMLdata = async url => {
-    const data = await d3.json(url);
-    return data;
-  };
-
-  // Saving a function in a varaible 'printResult' and 'printResult' is a func from above
-  const printResult = async patientID => {
-    // Saving results from promise that fethes the data by passing flask url as an argument into var
-    const data = await Promise.all([
-      await fetchMLdata(`/analyze/${patientID}`),
-      await fetchMLdata(`/predict/${patientID}`)
-    ]);
-    console.log(data); // Save results in the objects
-
-    uwResult = data[0];
-    cResult = data[1]; // if statement to return the diagnosis
-    if (uwResult === "Malignant" || cResult === "Malignant") {
-      console.log("Malignant");
-    } else {
-      console.log("Benigh");
-    }
-  };
-
+  // console.log(final);
+  resultDisplay.html(final);
+};
 
 // Create drop down
 createDropdown();
